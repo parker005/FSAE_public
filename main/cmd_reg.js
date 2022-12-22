cc2f51c01773046dbb0645fcdd9b0ea3e4461a1c
parker <90803531+parker005@users.noreply.github.com>
2022-12-21 21:54:31 -0500

    Create cmd_reg.js

---
+++ main/cmd_reg.js
@@ -0,0 +1,15 @@
+const { token, guildId, clientId } = require('./config.json')
+const { REST, SlashCommandBuilder, Routes } = require('discord.js')
+
+
+const commands = [
+    new SlashCommandBuilder().setName('yo').setDescription("replies hi")
+]
+
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands})
+    .then((data) => console.log(`Successfully registered ${data.length} application commands`))
+    .catch(console.error)
